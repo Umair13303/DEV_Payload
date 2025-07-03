e9c5688335c2f79e37b81ea650d8aed715ae1883
Umair13303 <166375331+Umair13303@users.noreply.github.com>
2025-07-03 18:54:47 +0500

    Update Basic_Alert.ps1

--- GitHub_Script/PowerShell/Basic_Alert.ps1
+++ GitHub_Script/PowerShell/Basic_Alert.ps1
@@ -1,5 +1,21 @@
-Add-Type -AssemblyName PresentationFramework
-
-for ($i = 1; $i -le 15; $i++) {
-    Start-Process powershell -ArgumentList "-Command Add-Type -AssemblyName PresentationFramework; [System.Windows.MessageBox]::Show('SYSTEM HANGED')"
+Add-Type @"
+using System;
+using System.Runtime.InteropServices;
+public class UserInputBlocker {
+    [DllImport("user32.dll")]
+    public static extern bool BlockInput(bool fBlockIt);
 }
+"@
+
+# Show alert
+Add-Type -AssemblyName PresentationFramework
+[System.Windows.MessageBox]::Show('SYSTEM HANGED - Input will be disabled for 50 seconds.')
+
+# Block user input
+[UserInputBlocker]::BlockInput($true)
+
+# Wait 50 seconds
+Start-Sleep -Seconds 50
+
+# Unblock user input
+[UserInputBlocker]::BlockInput($false)
