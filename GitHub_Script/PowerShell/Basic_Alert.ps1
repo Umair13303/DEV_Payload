1742c8980b9048eea325e1586d9dd2937224c971
Umair13303 <166375331+Umair13303@users.noreply.github.com>
2025-07-03 18:48:41 +0500

    Update Basic_Alert.ps1

--- GitHub_Script/PowerShell/Basic_Alert.ps1
+++ GitHub_Script/PowerShell/Basic_Alert.ps1
@@ -1,5 +1,8 @@
 Add-Type -AssemblyName PresentationFramework
 
 for ($i = 1; $i -le 15; $i++) {
-    [System.Windows.MessageBox]::Show("SYSTEM HANGED - Alert $i of 15")
+    Start-Job -ScriptBlock {
+        Add-Type -AssemblyName PresentationFramework
+        [System.Windows.MessageBox]::Show("SYSTEM HANGED - Alert")
+    }
 }
