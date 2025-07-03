f69b66caed8bb84820c30060e4baa223da018e1b
Umair13303 <166375331+Umair13303@users.noreply.github.com>
2025-07-03 16:52:56 +0500

    Update Basic_Alert.ps1

--- GitHub_Script/PowerShell/Basic_Alert.ps1
+++ GitHub_Script/PowerShell/Basic_Alert.ps1
@@ -1,2 +1,2 @@
 Add-Type -AssemblyName PresentationFramework
-[System.Windows.MessageBox]::Show('✅ Mehmood Bhai ITNA PURANY HUMARY SENIOR MANAGER HAIN!!!')
+[System.Windows.MessageBox]::Show('✅ Mehmood Bhai!!!')
