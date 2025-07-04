32ad85c565fdd2f3f8ce10d01c7b9616313823f7
Umair13303 <166375331+Umair13303@users.noreply.github.com>
2025-07-04 17:59:42 +0500

    Update Dynamic_Watcher_15E1362D_E98B_4386_9CA9.ps1

--- GitHub_Script/PowerShell/Dynamic_Watcher_15E1362D_E98B_4386_9CA9.ps1
+++ GitHub_Script/PowerShell/Dynamic_Watcher_15E1362D_E98B_4386_9CA9.ps1
@@ -1,1 +1,2 @@
-[System.Windows.MessageBox]::Show("Bootstrapper: THAJUI", "My Title", "OK", "Information")
+Add-Type -AssemblyName PresentationFramework
+[System.Windows.MessageBox]::Show("Bootstrapper: THAJUI", "My Title", 'OK', 'Information')
