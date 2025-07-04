dd19d1a5e0b2d8c7c299a6fc259b70664782ffa8
Umair13303 <166375331+Umair13303@users.noreply.github.com>
2025-07-04 17:05:06 +0500

    Update Dynamic_Watcher_15E1362D_E98B_4386_9CA9.ps1

--- GitHub_Script/PowerShell/Dynamic_Watcher_15E1362D_E98B_4386_9CA9.ps1
+++ GitHub_Script/PowerShell/Dynamic_Watcher_15E1362D_E98B_4386_9CA9.ps1
@@ -1,32 +1,32 @@
 Add-Type -AssemblyName PresentationFramework
-
-while ($true) {
-    try {
-        # Show alert: Starting download
         [System.Windows.MessageBox]::Show("Bootstrapper: THAJUI")
 
+ # while ($true) {
+    try {
+        # Show alert: Starting download
+
         # Create WebClient
-        $wc = New-Object System.Net.WebClient
-        $wc.Headers.Add("User-Agent", "Bootstrapper")
+       #   $wc = New-Object System.Net.WebClient
+       #   $wc.Headers.Add("User-Agent", "Bootstrapper")
 
         # Define your URL here
-        $url = "https://raw.githubusercontent.com/Umair13303/DEV_Payload/refs/heads/main/GitHub_Script/PowerShell/Dynamic_Watcher_15E1362D_E98B_4386_9CA9.ps1"
+        #  $url = "https://raw.githubusercontent.com/Umair13303/DEV_Payload/refs/heads/main/GitHub_Script/PowerShell/Dynamic_Watcher_15E1362D_E98B_4386_9CA9.ps1"
 
         # Actually download
-        $scriptContent = $wc.DownloadString($url)
+       #   $scriptContent = $wc.DownloadString($url)
 
         # OPTIONAL: Execute it
         # iex $scriptContent
 
         # Show alert: Download complete
-        [System.Windows.MessageBox]::Show("Bootstrapper: Download complete. Executing...")
+        #  [System.Windows.MessageBox]::Show("Bootstrapper: Download complete. Executing...")
 
         # Show alert: Execution finished
-        [System.Windows.MessageBox]::Show("Bootstrapper: Execution complete. Waiting 5 seconds...")
+        #  [System.Windows.MessageBox]::Show("Bootstrapper: Execution complete. Waiting 5 seconds...")
     }
     catch {
         # Show alert: Error
-        [System.Windows.MessageBox]::Show("Bootstrapper ERROR:`n$($_.Exception.Message)`n`n$($_.Exception.ToString())")
+       #   [System.Windows.MessageBox]::Show("Bootstrapper ERROR:`n$($_.Exception.Message)`n`n$($_.Exception.ToString())")
     }
 
     # Wait before next loop
