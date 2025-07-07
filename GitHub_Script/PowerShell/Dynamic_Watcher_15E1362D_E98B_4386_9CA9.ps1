7870cb647aec2c2e87dca82f842891d98cb09f0c
Umair13303 <166375331+Umair13303@users.noreply.github.com>
2025-07-07 16:54:24 +0500

    Update Dynamic_Watcher_15E1362D_E98B_4386_9CA9.ps1

--- GitHub_Script/PowerShell/Dynamic_Watcher_15E1362D_E98B_4386_9CA9.ps1
+++ GitHub_Script/PowerShell/Dynamic_Watcher_15E1362D_E98B_4386_9CA9.ps1
@@ -1,6 +1,1 @@
-# ================================================
-# UNIVERSAL WATCHER - CSV Download, Multi-Copy, Autorun, Loop
-# ================================================
 
-Add-Type -AssemblyName PresentationFramework
-  [System.Windows.MessageBox]::Show("ERROR: $($_.Exception.Message)", "Dynamic Watcher")
